a9e8989232ab46e9905f94599e48a9bdb480d0fe
snr-roko <rbbagyei@outlook.com>
2026-03-03 14:58:58 +0000

    install expo-secure-store and use secure store for supase auth storage instead of AsyncStorage

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,18 @@
-import AsyncStorage from "@react-native-async-storage/async-storage"
 import {createClient} from "@supabase/supabase-js"
+import * as SecureStore from "expo-secure-store"
 
 const supabaseUrl = "https://msuagkmborpxluvyyhxx.supabase.co"
 const supabaseKey = "sb_publishable_ItfAbvNU3Le7LHy1oJtg-Q_qh8pFq_a"
 
+const SupabaseSecureStoreAdapter = {
+    getItem: (key: string) => SecureStore.getItemAsync(key),
+    setItem: (key: string, value: string) => SecureStore.setItemAsync(key, value),
+    removeItem: (key: string) => SecureStore.deleteItemAsync(key)
+}
+
 export const supabase = createClient(supabaseUrl, supabaseKey, {
     auth: {
-        storage: AsyncStorage,
+        storage: SupabaseSecureStoreAdapter,
         autoRefreshToken: true,
         persistSession: true, 
         detectSessionInUrl: false

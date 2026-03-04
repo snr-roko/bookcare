cf201e69d850a27e395e3d889a60ac224806fd53
snr-roko <rbbagyei@outlook.com>
2026-03-04 22:05:32 +0000

    Refactor code structure for improved readability and maintainability

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,13 +1,113 @@
-import { Box } from "@/components/ui/box"
-import { Text } from "react-native"
+import { KeyboardAvoidingView, Platform, Text, TextInput, View} from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
+import {useForm, Controller} from "react-hook-form"
+import { LoginFormType } from "@/src/types"
+import {zodResolver} from "@hookform/resolvers/zod"
+import { LoginSchema } from "@/src/schemas"
+import {Ionicons} from "@expo/vector-icons"
+import { colors } from "@/src/constants"
+import { Button, ButtonText, ButtonSpinner } from "@/components/ui/button"
+import { supabase } from "@/src/lib/supabase"
+import { useState } from "react"
 
 const LoginScreen = () => {
+
+    const [isLoggingIn, setIsLoggingIn] = useState<boolean>(false)
+    const [showPassword, setShowPassword] = useState<boolean>(false)
+
+    const {control, handleSubmit, reset} = useForm<LoginFormType>({
+        defaultValues: {
+            "email": "",
+            "password": ""
+        },
+        mode: "onSubmit",
+        resolver: zodResolver(LoginSchema)
+    })
+
+    const logintoSupabase = async (email: string, password: string) => {
+        try{
+            setIsLoggingIn(true)
+            const response = await supabase.auth.signInWithPassword({email, password})
+            console.log(response)
+        } catch(error) {
+            console.log(error)
+        } finally {
+            setIsLoggingIn(false)
+            reset()
+        }
+    }
+
     return (
-        <SafeAreaView className="flex-1">
-            <Box className="flex-1 items-center justify-center bg-white">
-                <Text className="text-xl font-bold text-blue-500">Login</Text>
-            </Box>
+        <SafeAreaView className="flex-1 py-20 px-5 bg-bookcare-cream dark:bg-bookcare-darkBg">
+            <KeyboardAvoidingView className="gap-10 flex-1" behavior="height">
+                <View>
+                    <Text className="text-bookcare-textMuted text-2xl ">Welcome Back</Text>
+                    <Text className="text-bookcare-primary text-3xl">Login to Bookcare</Text>
+                </View>
+                <Controller
+                    name="email"
+                    control={control}
+                    render={({field, fieldState}) => {
+                        return (
+                            <View className="gap-2">
+                                <Text className="text-xl text-bookcare-textDark dark:text-bookcare-darkText" >Email</Text>
+                                <TextInput
+                                        inputMode="email"
+                                        onChangeText={field.onChange}
+                                        onBlur={field.onBlur}
+                                        value={field.value}
+                                        placeholder="john.doe@bookcare.com"
+                                        placeholderTextColor={colors.textMuted}
+                                        className="p-4 border border-bookcare-mid rounded-sm h-15 text-bookcare-textDark dark:text-bookcare-darkText"         
+                                    />
+                                
+                                <Text className="text-bookcare-textMuted">A valid email should have @ and .com/.org</Text>
+                                {fieldState.error && ( 
+                                    <View className="flex-row items-center gap-1">
+                                        <Ionicons name="alert-circle-outline" color={colors.error} />
+                                        <Text className="text-bookcare-error">{fieldState.error.message}</Text>
+                                    </View>
+                                )
+                                }
+                            </View>
+                        )
+                    }}
+                />
+                <Controller
+                    name="password"
+                    control={control}
+                    render={({field, fieldState}) => {
+                        return (
+                            <View className="gap-2">
+                                <Text className="text-xl text-bookcare-textDark dark:text-bookcare-darkText" >Password</Text>
+                                <TextInput
+                                        secureTextEntry={true}                                        
+                                        onChangeText={field.onChange}
+                                        onBlur={field.onBlur}
+                                        value={field.value}
+                                        placeholder="********************"
+                                        placeholderTextColor={colors.textMuted}
+                                        className="p-4 border border-bookcare-mid rounded-sm h-15 text-bookcare-textDark dark:text-bookcare-darkText"         
+                                    />
+                                
+                                <Text className="text-bookcare-textMuted">Password should be atleast 8 characters</Text>
+                                {fieldState.error && ( 
+                                    <View className="flex-row items-center gap-1">
+                                        <Ionicons name="alert-circle-outline" color={colors.error} />
+                                        <Text className="text-bookcare-error">{fieldState.error.message}</Text>
+                                    </View>
+                                )
+                                }
+                                <Button variant="link" className="self-end"><ButtonText className="text-bookcare-textMuted">Forgot Password?</ButtonText></Button>
+                            </View>
+                        )
+                    }}
+                />
+                <Button className="bg-bookcare-primary" size="xl" onPress={handleSubmit(({email, password}) => {logintoSupabase(email, password)})}>
+                    {isLoggingIn ? <ButtonSpinner color={colors.darkText} /> : <ButtonText size="xl" className="text-bookcare-darkText">Login</ButtonText>}
+                </Button>
+                <Button variant="link"><ButtonText className="text-bookcare-textMuted">Don't have an account? Register</ButtonText></Button>
+            </KeyboardAvoidingView>
         </SafeAreaView>
     )
 }
--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,13 +1,12 @@
-import { Box } from "@/components/ui/box"
-import { Text } from "react-native"
+import { Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 
 const ExploreScreen = () => {
     return (
         <SafeAreaView>
-            <Box className="flex-1 items-center justify-center bg-white">
+            <View className="flex-1 items-center justify-center bg-white">
                 <Text>Home</Text>
-            </Box>
+            </View>
         </SafeAreaView>
     )
 }

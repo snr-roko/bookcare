8f3e7befab9a48292919098c0f450a9bdb0d2a91
snr-roko <rbbagyei@outlook.com>
2026-03-06 06:15:43 +0000

    Add logout functionality to ExploreScreen: implement logoutUser function and button

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,11 +1,17 @@
+import { Button, ButtonText } from "@/components/ui/button"
+import { supabase } from "@/src/lib/supabase"
 import { Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 
 const ExploreScreen = () => {
+    const logoutUser = async() => {
+        await supabase.auth.signOut()
+    }
     return (
         <SafeAreaView>
             <View className="flex-1 items-center justify-center bg-white">
                 <Text>Home</Text>
+                <Button onPress={logoutUser}><ButtonText>Logout</ButtonText></Button>
             </View>
         </SafeAreaView>
     )

1a6c0fb013ce1f60b783989c4234d3c796abc8e4
snr-roko <rbbagyei@outlook.com>
2026-03-24 14:00:09 +0000

    Add order types to be used on order and checkout confirmation screen

---
+++ src/types/orderTypes.ts
@@ -0,0 +1,36 @@
+export type OrderItemType = {
+    id?: number
+    title: string
+    authorName: string
+    coverUrl: string
+    price: number
+    quantity: number
+    orderId: number
+}
+
+export type Order = {
+    id?: number
+    numberOfBooks: number
+    totalAmount: number
+    orderDate?: string
+    deliveryDate: string
+    userId: number
+}
+
+export type SupabaseOrder = {
+    id?: number
+    number_of_books: number
+    total_amount: number
+    delivery_date: string
+    user_id: number
+}
+
+export type SupabaseOrderItem = {
+    id?: number
+    author_name: string
+    cover_url: string,
+    order_id: number,
+    price: number,
+    quantity: number,
+    title: string
+}
